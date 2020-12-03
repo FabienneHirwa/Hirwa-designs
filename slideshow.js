068072176485efdd40b8e0743511abfc5bb43298
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-12-03 14:32:44 -0500

    init

--- slideshow.js
+++ slideshow.js
@@ -1,25 +1,26 @@
 
-var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+// var slideshows = document.querySelectorAll('[data-component="slideshow"]');
   
   
-  slideshows.forEach(initSlideShow);
+//   slideshows.forEach(initSlideShow);
 
-  function initSlideShow(slideshow) {
+//   function initSlideShow(slideshow) {
 
-    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); 
-    var index = 0, time = 3000;
-    slides[index].classList.add('active');  
+//     var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); 
+//     var index = 0, time = 3000;
+//     slides[index].classList.add('active');  
     
-    setInterval( () => {
-      slides[index].classList.remove('active');
-      
-     
-      index++;
+//     setInterval( () => {
+//       slides[index].classList.remove('active');
+//       index++;
       
       
-      if (index === slides.length) index = 0; 
+//       if (index === slides.length) index = 0; 
       
-      slides[index].classList.add('active');
+//       slides[index].classList.add('active');
 
-    }, time);
-  }
+//     }, time);
+//   }
+$('.carousel').carousel({
+  interval: 2000
+})
